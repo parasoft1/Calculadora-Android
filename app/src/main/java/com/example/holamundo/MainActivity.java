1c3e9149f41e7578a27d7f2900e60e4f5d9ba3bd
Pablo Romo <pablodress@gmail.com>
2019-08-21 20:37:28 -0500

    calculadora

---
+++ app/src/main/java/com/example/holamundo/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText numero1, numero2;
+    private TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        numero1 = (EditText)findViewById(R.id.txtnumero1);
+        numero2 = (EditText)findViewById(R.id.txtnumero2);
+
+        resultado = (TextView)findViewById(R.id.lblresultado);
+    }
+
+    public void sumar (View view){
+        double num1=Double.parseDouble(numero1.getText().toString());
+        double num2=Double.parseDouble(numero2.getText().toString());
+        resultado.setVisibility(View.VISIBLE);
+        resultado.setText(String.valueOf(num1+num2));
+    }
+
+    public void restar (View view){
+        double num1=Double.parseDouble(numero1.getText().toString());
+        double num2=Double.parseDouble(numero2.getText().toString());
+        resultado.setVisibility(View.VISIBLE);
+        resultado.setText(String.valueOf(num1-num2));
+    }
+
+    public void multiplicar (View view){
+        double num1=Double.parseDouble(numero1.getText().toString());
+        double num2=Double.parseDouble(numero2.getText().toString());
+        resultado.setVisibility(View.VISIBLE);
+        resultado.setText(String.valueOf(num1*num2));
+    }
+
+    public void dividir (View view){
+        double num1=Double.parseDouble(numero1.getText().toString());
+        double num2=Double.parseDouble(numero2.getText().toString());
+        resultado.setVisibility(View.VISIBLE);
+        resultado.setText(String.valueOf(num1/num2));
+    }
+
+
+}
